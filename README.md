e1013c3eb85068b11ffdf39038d5e3eafa67cb9e
Tyler Scott <off.nadir.insight@gmail.com>
2021-07-17 20:12:18 -0500

    add fibonacci solutions

--- README.md
+++ README.md
@@ -37,3 +37,14 @@ A palindrome is a string that is spelled the same both forwards and backwards, u
 palindromes('racecar') // true
 palindromes('tacos') // false
 ```
+
+### Exercise 10 - Fibonacci
+
+Create a function that returns a specific member of the Fibonacci sequence:
+
+> A series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers. The simplest is the series 1, 1, 2, 3, 5, 8, etc.
+
+```javascript
+fibonacci(4) // returns the 4th member of the series: 3  (1, 1, 2, 3)
+fibonacci(6) // returns 8
+```
--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,34 @@
-const fibonacci = function() {
-
+const fibonacci = function(counter) {
+  if (counter < 0) {
+    return "OOPS"
+  }
+  if (counter == 1) {
+    return 1;
+  }
+  if (counter == 2) {
+    return 1;
+  }
+  return fibonacci(counter - 1) + fibonacci(counter - 2)
 };
+
+/* alternate approach that came up in checking solution */
+/* "memoization" - keeps the computed result stored in an array for future reference */
+/* said to be of time complexity O(n) where recursion is O(2^n) */
+
+// function fibonacci(index, cache = []) {
+//   if (index < 0) {
+//     return "OOPS"
+//   }
+//   if (cache[index]) {
+//     return cache[index] 
+//   }
+//   else {
+//     if (index < 3) return 1
+//     else {
+//       cache[index] = fibonacci(index - 1, cache) + fibonacci(index - 2, cache)
+//     }
+//   }
+//   return cache[index];
+// }
 
 module.exports = fibonacci;
